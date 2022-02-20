fd897638e997bc7790d8c739af4bcfafb665040d
RAKIB <rakibislamdev@gmail.com>
2022-02-21 01:35:53 +0600

    switch and while loop added

---
+++ loops/while_loop.php
@@ -0,0 +1,9 @@
+<?php
+
+$x = 0;
+
+while ($x <= 10) {
+    echo $x;
+    echo "<br>";
+    $x++;
+}
---
+++ switch.php
@@ -0,0 +1,23 @@
+<?php
+$age = 25;
+
+switch ($age) {
+    case '12':
+        echo "You are 12 years old.<br>";
+        break;
+
+    case '20';
+        echo "Your are 20 years old.<br>";
+        break;
+    case '25';
+        echo "You are 25 years old.<br>";
+        break;
+
+    case '50';
+        echo "Your are 50 years old.<br>";
+        // break;
+
+    default:
+        echo "You are retired.<br>";
+        break;
+}
