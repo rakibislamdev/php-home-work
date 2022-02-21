666ca9ef28dbcc4b17019df79bc3019c59255012
RAKIB <rakibislamdev@gmail.com>
2022-02-22 02:08:07 +0600

    title add in all file

--- cookie_session/destroy_session.php
+++ cookie_session/destroy_session.php
@@ -7,3 +7,21 @@ session_unset();
 session_destroy();
 
 echo "<br> You have been logged out";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DESTROYED SESSION</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
--- cookie_session/session.php
+++ cookie_session/session.php
@@ -6,3 +6,21 @@ $_SESSION['username'] = "Rakib";
 $_SESSION['favfruit'] = "Apple";
 
 echo "We have saved your session";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SESSION IN PHP</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
