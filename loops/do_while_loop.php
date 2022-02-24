3aa44a3563bc68594fcec1cdf871be47c7ba8b69
RAKIB <rakibislamdev@gmail.com>
2022-02-25 01:57:31 +0600

    do_while_loop file added

---
+++ loops/do_while_loop.php
@@ -0,0 +1,8 @@
+<?php
+
+$x = 1;
+do {
+    echo "The number is: $x";
+    echo "<br>";
+    $x++;
+} while ($x <= 10);
---
+++ loops/for_loop.php
@@ -0,0 +1,13 @@
+<?php
+echo "For loop using by increment: <br>";
+for ($i = 1; $i <= 10; $i++) {
+    echo "$i";
+    echo "<br>";
+}
+
+echo "For loop using by decrement: <br>";
+
+for ($a = 10; $a >= 0; $a--) {
+    echo $a;
+    echo "<br>";
+}
